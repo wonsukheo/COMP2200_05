506e2a49e1a3b2aff3e0d45f3e0b54ee97f0edbd
wonsukheo <62510983+wonsukheo@users.noreply.github.com>
2021-05-27 02:06:26 -0500

    error fixing

--- Lab4/my_string.c
+++ Lab4/my_string.c
@@ -37,12 +37,22 @@ int index_of(const char* str, const char* word)
     size_t word_len = get_str_len(word);
     size_t count = 0;
 
+    if (word_len == 0) {
+        return 0;
+    }
     while (*str_ptr != '\0') {
-        if (*str_ptr++ == *word_ptr++) {
+        if (*str_ptr == *word_ptr) {
             count++;
+            word_ptr++;
+            str_ptr++;
         } else { 
-            word_ptr = word;
             count = 0;
+            word_ptr = word;
+
+            if (*str_ptr++ == *word_ptr) {
+                count++;
+                word_ptr++;
+            }
         }
         
         if (count == word_len) {
@@ -59,16 +69,16 @@ void reverse_by_words(char* str)
     char* word_ptr = str;
 
     while (*str_ptr != '\0') {
-         if (*str_ptr == ' ') {
-             *str_ptr = '\0';
-             reverse(word_ptr);
+        if (*str_ptr == ' ') {
+            *str_ptr = '\0';
+            reverse(word_ptr);
          
-             *str_ptr = ' ';
-             str_ptr++;
-             word_ptr = str_ptr;
-         } else {
-             str_ptr++;
-         }
+            *str_ptr = ' ';
+            str_ptr++;
+            word_ptr = str_ptr;
+        } else {
+            str_ptr++;
+        }
    }
    reverse(word_ptr);
 }
@@ -82,22 +92,29 @@ char* tokenize(char* str_or_null, const char* delims)
     if (str_or_null == NULL) {
         str_ptr = s_original_str;
     }
-    
+    /*
     if (str_ptr == NULL && s_original_str == NULL) {
         return NULL;
     }
-
+    */
     while (*str_ptr != '\0') {
         size_t i;
         for (i = 0; i < delims_count; i++) {
             if (*str_ptr == delims[i]) {
                 *str_ptr = '\0';
                 s_original_str = str_ptr + 1;
+
+                if (str_ptr_count == 0) {
+                    return tokenize(NULL, delims);
+                }
                 return str_ptr - str_ptr_count;
             }
         }
         str_ptr_count++;
         str_ptr++;
+    }
+    if (*str_ptr == '\0' && str_ptr_count != 0) {
+        return str_ptr - str_ptr_count;
     }
     return NULL;
 }
@@ -111,11 +128,11 @@ char* reverse_tokenize(char* str_or_null, const char* delims)
     if (str_or_null == NULL) {
         str_ptr = s_original_str;
     }
-
+    /*
     if (str_ptr == NULL && s_original_str == NULL) {
         return NULL;
     }
-
+    */
     while (*str_ptr != '\0') {
         size_t i;
         for (i = 0; i < delims_count; i++) {
@@ -123,11 +140,19 @@ char* reverse_tokenize(char* str_or_null, const char* delims)
                 *str_ptr = '\0';
                 s_original_str = str_ptr + 1;
                 reverse(str_ptr - str_ptr_count);
+
+                if (str_ptr_count == 0) {
+                    return reverse_tokenize(NULL, delims);
+                }
                 return str_ptr - str_ptr_count;
             }
         }
         str_ptr_count++;
         str_ptr++;
+    }
+    if (*str_ptr == '\0' && str_ptr_count != 0) {
+        reverse(str_ptr - str_ptr_count);
+        return str_ptr - str_ptr_count;
     }
     return NULL;       
 }
